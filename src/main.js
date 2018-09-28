02cdb5c9a613e8789ed6a5eba6b18dd240a002a0
steadmengyan <2691976568@qq.com>
2018-09-28 08:55:54 +0800

    xiwu_first

--- src/main.js
+++ src/main.js
@@ -1,7 +1,36 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+import "babel-polyfill"
+Vue.use(Vuex)
+Vue.use(VueRouter)
+const Verify = require('./components/verify/verify.vue')// 验证
+const router = new VueRouter({
+  routes:[
+    {
+      path:'/verify',
+      component:Verify.default,
+      name:"验证"
+    }
+  ]
+})
+const store = new Vuex.Store({
+  state:{
 
+  },
+  mutations:{
+    
+  },
+  actions:{
+
+  }
+})
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+  store,
+  components:{
+    App
+  }
 })
